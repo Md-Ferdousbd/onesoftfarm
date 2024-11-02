8392771b2bf0a09566c9220a7030143bbcacdecc
Md-Ferdousbd <onesoftwarefarm@gmail.com>
2024-11-02 17:42:12 +0600

    Add files via upload

---
+++ Calculator 31.102024.py
@@ -0,0 +1,23 @@
+num1= float(input("please enter first number: "))
+symbol= input("please input symbol, + ,-,*,/")
+num2= float(input("please enter second number: "))
+
+#result
+result= 0
+
+
+if symbol == '+':
+    result = num1 + num2
+elif symbol == '-':
+    redult= num1-num2
+elif symbol == '*':
+    result= num1 * num2
+elif symbol == '/':
+    result= num1 / num2
+else :
+    result= 'Not allowed'
+print("The result is ", result)
+print("thank you for using my first software ")
+print(" Developed by One Software Farm ")
+print("www.drmdferdous.com, WhatsApp: +8801681519799")
+
